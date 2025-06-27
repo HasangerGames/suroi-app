8e866de7230f3199ecbd799c1e63a033c03821e3
Zereshk <zereshk.dev@gmail.com>
2025-06-27 10:53:48 -0500

    feature: Add settings icon

--- composeApp/src/commonMain/kotlin/io/suroi/App.kt
+++ composeApp/src/commonMain/kotlin/io/suroi/App.kt
@@ -1,21 +1,24 @@
 package io.suroi
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
+import io.suroi.ui.theme.DarkTransparent
+import io.suroi.ui.theme.Gray
 import io.suroi.ui.theme.SuroiTheme
+import io.suroi.ui.theme.White
 import kotlinx.coroutines.launch
+import org.jetbrains.compose.resources.painterResource
+import suroi.composeapp.generated.resources.Res
+import suroi.composeapp.generated.resources.settings
 
 @Composable
 fun App() {
@@ -37,61 +40,103 @@ fun App() {
                 backgroundIsLoading = false
             }
         }
-        if (backgroundIsLoading || connecting) {
+        Box(modifier = Modifier.fillMaxSize().background(color = Gray)) {
+            if (backgroundIsLoading || connecting) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    CircularProgressIndicator(modifier = Modifier.size(96.dp))
+                }
+            } else {
+                AsyncImage(
+                    model = backgroundImageURL.value,
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop,
+                )
+            }
             Column(
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier
+                    .fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
-                CircularProgressIndicator(modifier = Modifier.size(96.dp))
-            }
-        } else {
-            AsyncImage(
-                model = backgroundImageURL.value,
-                contentDescription = null,
-                modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.Crop,
-            )
-        }
-        Column(
-            modifier = Modifier
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            if (showOfflineScreen) {
-                Text(text = "You're offline")
-                Button(onClick = {
-                    showOfflineScreen = false
-                }) {
-                    Text("Retry")
-                }
-            } else if (!showContent) {
-                Button(
-                    onClick = {
+                if (showOfflineScreen) {
+                    Text(text = "You're offline")
+                    Button(onClick = {
                         showOfflineScreen = false
-                        connecting = true
-                        coroutineScope.launch {
-                            if (isOnline()) {
-                                showContent = true
-                            } else {
-                                showOfflineScreen = true
+                    }) {
+                        Text("Retry")
+                    }
+                } else if (!showContent) {
+                    Button(
+                        onClick = {
+                            showOfflineScreen = false
+                            connecting = true
+                            coroutineScope.launch {
+                                if (isOnline()) {
+                                    showContent = true
+                                } else {
+                                    showOfflineScreen = true
+                                }
                             }
-                        }
-                        connecting = false
-                    },
-                    enabled = !connecting
-                ) {
-                    Text("Let's play")
+                            connecting = false
+                        },
+                        enabled = !connecting
+                    ) {
+                        Text("Let's play")
+                    }
+                }
+                AnimatedVisibility(showContent) {
+                    Webview(
+                        "https://suroi.io",
+                        modifier = Modifier.fillMaxSize(),
+                        script = "document.querySelector('.btn-kofi').style.display = 'none';",
+                        onURLChange = { showContent = false }
+                    )
                 }
             }
-            AnimatedVisibility(showContent) {
-                Webview(
-                    "https://suroi.io",
-                    modifier = Modifier.fillMaxSize(),
-                    script = "document.querySelector('.btn-kofi').style.display = 'none';",
-                    onURLChange = { showContent = false }
-                )
+            Box(
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .padding(16.dp)
+                    .background(
+                        color = DarkTransparent,
+                        shape = RoundedCornerShape(24.dp)
+                    )
+            ) {
+                IconButton(
+                    onClick = {
+                        println("settings clicked")
+                    },
+                    modifier = Modifier
+                        .size(48.dp)
+                        .padding(4.dp)
+                ) {
+                    Icon(
+                        painter = painterResource(Res.drawable.settings),
+                        contentDescription = "settings",
+                        tint = White,
+                        modifier = Modifier,
+                    )
+                }
+                IconButton(
+                    onClick = {
+                        println("settings clicked")
+                    },
+                    modifier = Modifier
+                        .size(48.dp)
+                        .padding(4.dp)
+                ) {
+                    Icon(
+                        painter = painterResource(Res.drawable.settings),
+                        contentDescription = "settings",
+                        tint = White,
+                        modifier = Modifier,
+                    )
+                }
             }
         }
     }
--- composeApp/src/commonMain/kotlin/io/suroi/ui/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/io/suroi/ui/theme/Theme.kt
@@ -1,42 +1,25 @@
 package io.suroi.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
 @Composable
 fun SuroiTheme(
-    darkMode: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkMode) DarkColorScheme else LightColorScheme
+    val colors = DarkColorScheme
     MaterialTheme(
         colorScheme = colors,
         content = content
     )
 }
 
-private val LightColorScheme = lightColorScheme(
-    primary = Orange,
-    onPrimary = Light,
-    primaryContainer = Light,
-    onPrimaryContainer = Dark,
-    surface = Light,
-    onSurface = Dark,
-    secondary = Blue,
-    onSecondary = Light,
-    tertiary = Purple,
-    onTertiary = Light,
-    error = Danger,
-    onError = Light,
-)
 
 private val DarkColorScheme = darkColorScheme(
     primary = Orange,
     onPrimary = Dark,
-    primaryContainer = DarkGray,
+    primaryContainer = Gray,
     onPrimaryContainer = Light,
     surface = DarkGray,
     onSurface = Light,
