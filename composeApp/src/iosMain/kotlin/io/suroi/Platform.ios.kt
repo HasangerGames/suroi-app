21b059b33ba6b791513e3dee0cfe3add8edffbe8
Zereshk <zereshk.dev@gmail.com>
2025-06-30 21:48:13 -0500

    feature: add script to iOS WKWebView

--- composeApp/src/iosMain/kotlin/io/suroi/Platform.ios.kt
+++ composeApp/src/iosMain/kotlin/io/suroi/Platform.ios.kt
@@ -4,10 +4,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.UIKitView
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.CoreGraphics.CGRectMake
 import platform.Foundation.*
 import platform.UIKit.UIView
-import platform.WebKit.WKWebView
+import platform.WebKit.*
 
+@OptIn(ExperimentalForeignApi::class)
 @Composable
 actual fun Webview(
     url: String,
@@ -17,7 +20,22 @@ actual fun Webview(
 ) {
     UIKitView(
         factory = {
-            val webView = WKWebView()
+            val userScript = WKUserScript(
+                source = script,
+                injectionTime = WKUserScriptInjectionTime.WKUserScriptInjectionTimeAtDocumentEnd,
+                forMainFrameOnly = true
+            )
+            val userContentController = WKUserContentController()
+            userContentController.addUserScript(userScript = userScript)
+            val configuration = WKWebViewConfiguration()
+            configuration.userContentController = userContentController
+
+            val webView = WKWebView(
+                frame = CGRectMake(
+                0.0, 0.0, 0.0, 0.0
+                ),
+                configuration = configuration
+            )
             webView.loadRequest(request = NSURLRequest(uRL = NSURL(string = url)))
             webView as UIView
         },
